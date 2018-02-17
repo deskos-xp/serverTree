f9d184dd03d93e787a6a62aa82980f9dc6379d4c
noguilinux <k.j.hirner.wisdom@gmail.com>
2018-02-17 16:39:13 +0000

    updated plugins

--- plugins/crypto.py
+++ plugins/crypto.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 import sys,os
 
-#plugins
-sys.path.insert(0,".")
-import colors
-color=colors.colors()
+try:
+    #plugins
+    sys.path.insert(0,".")
+    import colors
+    color=colors.colors()
+except:
+    print("WARN: colors lib could not be imported")
 
 from Crypto.Cipher import AES
 
@@ -34,19 +37,25 @@ class theCryptKeeper:
     def decrypt(self,key,file,ofile):
         cipher=AES.new(key)
         plaintext=b''
+        multi=int((os.stat(file).st_size/16)-1)
+        counter=0
         with open(file,'rb') as data:
+            odata=open(ofile,"wb")
             while True:
                 dt=data.read(16)
                 if not dt:
                     break
-                plaintext+=cipher.decrypt(dt)
-            odata=open(ofile,"wb")
-            endChar=plaintext[-1]
-            odata.write(plaintext[:-endChar])
+                plaintext=cipher.decrypt(dt)
+                if counter == multi:
+                    endChar=plaintext[-1]
+                    plaintext=plaintext[:-endChar]
+                odata.write(plaintext)
+                counter+=1
 
-'''
+
+"""
 keeper=theCryptKeeper()
 key=keeper.adjustKey("sixteen")
 ec=keeper.encrypt(key,"tarball.tgz","t.tmp")
 keeper.decrypt(key,"t.tmp","tarball.tgz.tmp")
-'''
+"""
