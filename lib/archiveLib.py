7266330fabc3d4a60c450ee9734b9e90b40c187b
NoGuiLinux <k.j.hirner.wisdom@gmail.com>
2018-02-28 10:29:41 -0500

    updates

--- lib/archiveLib.py
+++ lib/archiveLib.py
@@ -1,9 +1,75 @@
 #! /usr/bin/env python3
 import os,zipfile,shutil
 import tarfile
+import os,sys,subprocess as sp
 
 import colors
 color=colors.colors()
+
+class cpioShell:
+    fpaths=[]
+    oFile='default.cpio'
+    DST='tmp'
+    SRC="./bcard"
+    manifest='manifest.xml'
+    counterF=0
+    counterD=0
+    def prep(self):
+        if not os.path.exists(self.DST):
+            os.mkdir(self.DST)
+        if not os.path.exists(os.path.join(self.DST,self.manifest)):
+            shutil.copyfile(self.manifest,os.path.join(self.DST,self.manifest))
+        shutil.copytree(self.SRC,os.path.join(self.DST,os.path.split(self.SRC)[1]))
+
+    def getFnames(self):
+        #need a function that will auto-slash invalid chars for shell/bash
+        for root,dir,fnames in os.walk(self.DST,topdown=True):
+            for d in dir:
+                if os.path.isdir(os.path.join(root,d)):
+                    self.counterD+=1
+            for fname in fnames:
+                path=os.path.join(root,fname)
+                if os.path.exists(path):
+                    self.fpaths.append(path)
+                    if os.path.isfile(path):
+                        self.counterF+=1
+        msg="Files: {}\nDirectories: {}".format(self.counterF,self.counterD)
+        print(msg)
+
+    def createArchive(self):
+        self.prep()
+        self.getFnames()
+        if os.path.exists(self.oFile):
+            msg="oFile '{}' exists... deleting!".format(self.oFile)
+            exit(msg)
+            os.remove(self.oFile)
+        if not os.path.exists(self.SRC):
+            msg="SRC '{}' does not exist!"
+            exit(msg)
+        cmd='echo -e "'+'\n'.join(self.fpaths)+'" | cpio -o > '+self.oFile
+        data=sp.Popen(cmd,shell=True,stdout=sp.PIPE)
+        stdout,stderr=data.communicate()
+        if stdout != b'':
+            print(stdout)
+
+    def extractArchive(self):
+        cmd='cpio -id <'+self.ofile
+        #list file later to get topdir
+        #test to see if destination dir exists
+        ##if yes then exit with err
+        ##else begin extraction
+        data=sp.Popen(cmd,shell=True,stdout=sp.PIPE)
+        stdout,stderr=data.communicate()
+        print(stdout)
+
+#critical
+'''
+sd=cpioShell()
+sd.DST='tmp'
+sd.SRC='/home/carl/Documents'
+sd.oFile='Documents.cpio'
+sd.createArchive()
+'''
 
 class tarball:
     SRC=os.path.realpath(os.path.expanduser("~/Documents"))
